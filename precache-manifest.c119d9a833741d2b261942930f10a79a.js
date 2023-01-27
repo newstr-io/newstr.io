fe679d0ea9c0487e71bf03bfaadaf6b62959f52c
Liran Cohen <lcohen@getwrecked.com>
2023-01-27 14:20:03 -0500

    Updates

---
+++ precache-manifest.c119d9a833741d2b261942930f10a79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1759540dee7a9e4a3330",
+    "url": "/static/css/main.5fe69829.chunk.css"
+  },
+  {
+    "revision": "1759540dee7a9e4a3330",
+    "url": "/static/js/main.1759540d.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "ddb530a8135c7227fcc0",
+    "url": "/static/js/2.ddb530a8.chunk.js"
+  },
+  {
+    "revision": "38973534419a972cf74f",
+    "url": "/static/js/runtime~main.38973534.js"
+  },
+  {
+    "revision": "034641c12361b95f5c5b1137bd4650de",
+    "url": "/index.html"
+  }
+];
