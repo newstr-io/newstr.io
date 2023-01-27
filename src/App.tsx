3e918918e7e8a8bc2f9b5350bdfc4e443ad1cff6
Liran Cohen <lcohen@getwrecked.com>
2023-01-27 14:07:46 -0500

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopyright, faLinesLeaning } from '@fortawesome/free-solid-svg-icons';
